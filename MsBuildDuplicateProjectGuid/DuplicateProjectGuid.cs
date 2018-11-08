2e4b0ce1f7245992dc83c0ee7c4372125c5b693f
Ace Olszowka <aolszowka@gmail.com>
2018-11-08 15:32:25 -0700

    Initial commit of the MsBuildDuplicateProjectGuid Utility

---
+++ MsBuildDuplicateProjectGuid/DuplicateProjectGuid.cs
@@ -0,0 +1,65 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DuplicateProjectGuid.cs" company="Ace Olszowka">
+//  Copyright (c) Ace Olszowka 2018. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MsBuildDuplicateProjectGuid
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    internal static class DuplicateProjectGuid
+    {
+        internal static ConcurrentDictionary<string, ConcurrentBag<string>> Find(string targetDirectory)
+        {
+            IEnumerable<string> projectFiles = GetProjectsInDirectory(targetDirectory);
+
+            ConcurrentDictionary<string, string> distinctProjectGuids = new ConcurrentDictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+            ConcurrentDictionary<string, ConcurrentBag<string>> duplicatedGuids = new ConcurrentDictionary<string, ConcurrentBag<string>>(StringComparer.InvariantCultureIgnoreCase);
+
+            Parallel.ForEach(projectFiles, projectFile =>
+            {
+                string projectGuid = MSBuildUtilities.GetMSBuildProjectGuid(projectFile);
+
+                if (!distinctProjectGuids.TryAdd(projectGuid, projectFile))
+                {
+                    // The Key was duplicated
+
+                    // First try to create a new bag (assume its the first duplication we have)
+                    ConcurrentBag<string> duplicatedProjects = new ConcurrentBag<string>(new string[] { distinctProjectGuids[projectGuid] });
+
+                    if (!duplicatedGuids.TryAdd(projectGuid, duplicatedProjects))
+                    {
+                        duplicatedProjects = duplicatedGuids[projectGuid];
+                    }
+
+                    duplicatedProjects.Add(projectFile);
+                }
+            }
+            );
+
+            return duplicatedGuids;
+        }
+
+        /// <summary>
+        /// Gets all Project Files that are understood by this
+        /// tool from the given directory and all subdirectories.
+        /// </summary>
+        /// <param name="targetDirectory">The directory to scan for projects.</param>
+        /// <returns>All projects that this tool supports.</returns>
+        internal static IEnumerable<string> GetProjectsInDirectory(string targetDirectory)
+        {
+            HashSet<string> supportedFileExtensions = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase) { ".csproj", ".vbproj", ".synproj" };
+
+            return
+                Directory
+                .EnumerateFiles(targetDirectory, "*proj", SearchOption.AllDirectories)
+                .Where(currentFile => supportedFileExtensions.Contains(Path.GetExtension(currentFile)));
+        }
+    }
+}
---
+++ MsBuildDuplicateProjectGuid/Program.cs
@@ -0,0 +1,71 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Ace Olszowka">
+//  Copyright (c) Ace Olszowka 2018. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MsBuildDuplicateProjectGuid
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int errorCode = 0;
+
+            if (args.Any())
+            {
+                string command = args.First().ToLowerInvariant();
+
+                if (command.Equals("-?") || command.Equals("/?") || command.Equals("-help") || command.Equals("/help"))
+                {
+                    errorCode = ShowUsage();
+                }
+                else
+                {
+                    if (Directory.Exists(command))
+                    {
+                        string targetPath = command;
+                        errorCode = FindDuplicateGuids(targetPath);
+                    }
+                    else
+                    {
+                        string error = string.Format("The specified path `{0}` is not valid.", command);
+                        Console.WriteLine(error);
+                        errorCode = 1;
+                    }
+                }
+            }
+            else
+            {
+                // This was a bad command
+                errorCode = ShowUsage();
+            }
+
+            Environment.Exit(errorCode);
+        }
+
+        private static int ShowUsage()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int FindDuplicateGuids(string targetDirectory)
+        {
+            KeyValuePair<string, ConcurrentBag<string>>[] results = DuplicateProjectGuid.Find(targetDirectory).ToArray();
+
+            foreach (KeyValuePair<string, ConcurrentBag<string>> kvp in results)
+            {
+                string duplicatedProjects = string.Join(",", kvp.Value);
+                Console.WriteLine($"Key `{kvp.Key}` duplicated in projects: {duplicatedProjects}");
+            }
+
+            return results.Length;
+        }
+    }
+}
