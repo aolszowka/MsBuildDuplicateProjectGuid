7121c00ad66c422b70153e1ab7ded7fae510f4ea
Ace Olszowka <aolszowka@gmail.com>
2020-10-19 10:34:12 -0600

    Updated the script to forcably remove the package cache since --no-cache doesn't seem to work

--- build/Update-Package.bat
+++ build/Update-Package.bat
@@ -1,4 +1,15 @@
+@ECHO OFF
+
 pushd ..
+
+REM Clear any packages out of the local nuget package cache
+SET PACKAGE_CACHE_FOLDER=%userprofile%\.nuget\packages\msbuildduplicateprojectguid
+ECHO Attempting to Clean Existing Package Cache From
+IF EXIST "%PACKAGE_CACHE_FOLDER%" (
+RMDIR /Q /S "%PACKAGE_CACHE_FOLDER%"
+)
+
 dotnet tool uninstall msbuildduplicateprojectguid
-dotnet tool install msbuildduplicateprojectguid --add-source=nupkg --no-cache
+dotnet tool install --add-source=nupkg --no-cache msbuildduplicateprojectguid
+
 popd
