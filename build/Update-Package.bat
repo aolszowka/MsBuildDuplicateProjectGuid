f0825ebc38d2aa3d51d10a06d8e2328e021ee2e4
Ace Olszowka <aolszowka@gmail.com>
2020-10-23 15:32:58 -0600

    Update the Package Updater to be more generic and allow us to install pre-release versions

--- build/Update-Package.bat
+++ build/Update-Package.bat
@@ -1,15 +1,34 @@
 @ECHO OFF
 
+SET PACKAGE_NAME=msbuildduplicateprojectguid
+SET CURRENT_VERSION=2.0.0
+
 pushd ..
 
 REM Clear any packages out of the local nuget package cache
-SET PACKAGE_CACHE_FOLDER=%userprofile%\.nuget\packages\msbuildduplicateprojectguid
-ECHO Attempting to Clean Existing Package Cache From
+REM This is because `dotnet tool install --no-cache` appears to be broken?
+SET PACKAGE_CACHE_FOLDER=%userprofile%\.nuget\packages\%PACKAGE_NAME%
+ECHO Attempting to Clean Existing Package Cache From %PACKAGE_CACHE_FOLDER%
 IF EXIST "%PACKAGE_CACHE_FOLDER%" (
 RMDIR /Q /S "%PACKAGE_CACHE_FOLDER%"
 )
 
-dotnet tool uninstall msbuildduplicateprojectguid
-dotnet tool install --add-source=nupkg --no-cache msbuildduplicateprojectguid
+ECHO.
+ECHO Delete Existing Packs
+IF EXIST nupkg (
+RMDIR /q /s nupkg
+)
+
+ECHO.
+ECHO Building / Packing
+dotnet pack --configuration Debug --version-suffix "dev"
+
+ECHO.
+ECHO Uninstall Existing Tooling
+dotnet tool uninstall %PACKAGE_NAME%
+
+ECHO.
+ECHO Install the Latest Prerelease
+dotnet tool install --add-source=nupkg --no-cache %PACKAGE_NAME% --version=%CURRENT_VERSION%-dev
 
 popd
